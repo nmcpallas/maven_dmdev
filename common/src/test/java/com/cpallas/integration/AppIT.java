31c6fb274befa66c36c59e6cccf4a858135265a5
nmcpallas <nmcpallas@gmail.com>
2023-02-05 01:43:23 +0400

    [task-1] task done without jacoco

---
+++ common/src/test/java/com/cpallas/integration/AppIT.java
@@ -0,0 +1,13 @@
+package com.cpallas.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppIT {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
